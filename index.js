78fb31a77e1d377596a1d841680b0bdf55d46a94
Kwame Val Kraidy Solomon <valkraidy@gmail.com>
2023-12-17 13:36:44 -1200

    building first API project

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+//routes
+
+app.get('/',(req,res)=>{
+    res.send('hello node API')
+}
+
+)
+
+
+
+
+app.listen(3000,()=>{
+    console.log("Node API app is running on port 3000" )
+}
+)
